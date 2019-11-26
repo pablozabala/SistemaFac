3ed3791d8563726b41e861e81382a28a786014f1
pablozabala <pablozabala.jakemate@gmail.com>
2019-11-26 17:28:27 -0300

    rr
    adfdfasdfsrrfsdfdfadfasdfadf

---
+++ SistemaFact/SistemaFact/Clases/cArticulo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaFact.Clases
+{
+    public class cArticulo
+    {
+        public void Insertar(string Nombre,Double? Costo,Double? Precio,int? CodColor)
+        {
+            string sql = "insert into Articulo(";
+            sql = sql + "Nombre,Costo,Precio,CodColor)";
+            sql = sql + " Values(";
+            sql = sql + "'" + Nombre + "'";
+            if (Costo != null)
+                sql = sql + "," + Costo.ToString().Replace(",".".");
+            else
+                sql = sql + ",null";
+            if (Precio != null)
+                sql = sql + "," + Precio.ToString().Replace(",".".");
+            else
+                sql = sql + ",null";
+            if (CodColor != null)
+                sql = sql + "," + CodColor.ToString();
+            else
+                sql = sql + ",null";
+            sql = sql + ")";
+            cDb.Grabar(sql);
+        }
+    }
+}
